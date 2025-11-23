04065abdeb5ba048e6e116d30d57ef8d77f9127d
BlueAmulet <43395286+BlueAmulet@users.noreply.github.com>
2025-11-23 13:29:35 -0700

    Faster remove_empty_vertex_groups
    Process all vertices once

--- retarget_script2_en.py
+++ retarget_script2_en.py
@@ -3198,19 +3198,17 @@ def remove_empty_vertex_groups(mesh_obj: bpy.types.Object) -> None:
     if not mesh_obj.type == 'MESH' or not mesh_obj.vertex_groups:
         return
 
+    used_vertex_groups = {i: False for i in range(len(mesh_obj.vertex_groups))}
+    for vert in mesh_obj.data.vertices:
+        for g in vert.groups:
+            if g.weight > 0.0005:
+                used_vertex_groups[g.group] = True
+
     groups_to_remove = []
-    for vgroup in mesh_obj.vertex_groups:
-        has_weights = False
-        for vert in mesh_obj.data.vertices:
-            weight_index = vgroup.index
-            for g in vert.groups:
-                if g.group == weight_index and g.weight > 0.0005:
-                    has_weights = True
-                    break
-            if has_weights:
-                break
-        if not has_weights:
-            groups_to_remove.append(vgroup.name)
+
+    for i, used in used_vertex_groups.items():
+        if not used:
+            groups_to_remove.append(mesh_obj.vertex_groups[i].name)
 
     for group_name in groups_to_remove:
         if group_name in mesh_obj.vertex_groups:
