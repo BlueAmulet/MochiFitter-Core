63850bc244030090945c32b707a8b1e4bc68274b
BlueAmulet <43395286+BlueAmulet@users.noreply.github.com>
2025-11-23 13:22:48 -0700

    Remove unused cKDTree

--- retarget_script2.py
+++ retarget_script2.py
@@ -15132,7 +15132,7 @@ def create_distance_normal_based_vertex_group(body_obj, cloth_obj, distance_thre
     #     kd.insert(center, face_index)
 
     # kd.balance()
-    kd = cKDTree(face_centers)
+    # kd = cKDTree(face_centers)
 
     # 衣装メッシュの頂点に対してKDTreeを構築（新しい実装用）
     vertex_positions = []
